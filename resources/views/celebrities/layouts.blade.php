22371ed63e8fe56f616e896be355d5bb94738ae2
moustaphahcse <moustapha.camara@helloce.fr>
2023-12-21 14:45:17 +0100

    feat(celebrity): create files & index.blade content

---
+++ resources/views/celebrities/layouts.blade.php
@@ -0,0 +1,3 @@
+<div class="container">
+    @include('celebrities.index')
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,23 @@
 <!doctype html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HomePage</title>
-    @vite('resources/js/app.js')
+    <title>Projet Kiki</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
+    @vite('resources/js/app.js')
 </head>
 <body>
-<h1>Celebrities</h1>
-    <p>Welcome</p>
+<h1>homepage</h1>
+@include('celebrities.layouts')
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+        crossorigin="anonymous"></script>
 </body>
 </html>
