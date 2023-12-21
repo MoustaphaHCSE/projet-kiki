f10253a55f3806265d1b629feb05788b8260aa20
Moustapha <147492963+MoustaphaHCSE@users.noreply.github.com>
2023-12-21 17:53:44 +0100

    develop (#2)
    * feat(celebrity): model & mcr
    
    * feat(celebrity): Controller methods, fillable data & index
    
    * feat(celebrity): store & update requests
    
    * feat(celebrity): boostrap 5 (paginator)
    
    * feat(celebrity): create files & index.blade content
    
    * feat(celebrity): view - create
    
    * feat(celebrity): crud

---
+++ resources/views/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    @include('celebrities.index')
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CelebrityController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::resource('celebrities', CelebrityController::class);
