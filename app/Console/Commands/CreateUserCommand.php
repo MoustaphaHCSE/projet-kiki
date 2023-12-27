4371bc081ed9efebe6d71c45502466f89614d13c
moustaphahcse <moustapha.camara@helloce.fr>
2023-12-27 16:24:16 +0100

    feat(command): progress bar

--- app/Console/Commands/CreateUserCommand.php
+++ app/Console/Commands/CreateUserCommand.php
@@ -13,7 +13,7 @@ class CreateUserCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'user:create {name} {email} {password?}';
+    protected $signature = 'user:create {--count=}';
 
     /**
      * The console command description.
@@ -27,14 +27,24 @@ class CreateUserCommand extends Command
      */
     public function handle()
     {
-        $name = $this->argument('name');
-        $email = $this->argument('email');
-        $password = $this->argument('password') ?? Hash::make('password');
-        User::create([
-            'name' => $name,
-            'email' => $email,
-            'password' => $password,
-        ]);
-        $this->info(sprintf('Success creating user: %s and his email %s.', $name, $email));
+        $count = $this->option('count');
+
+        $bar = $this->output->createProgressBar($count);
+        $bar->start();
+        for ($i = 1; $i <= $count; $i++) {
+//            $name = $this->argument('name');
+//            $email = $this->argument('email');
+//            $password = $this->argument('password') ?? Hash::make('password');
+            $name = fake()->name();
+            $mail = preg_replace('/[^a-z]/', '', strtolower($name));
+            User::create([
+                'name' => $name,
+                'email' => $mail . '@gmail.com',
+                'password' => Hash::make('password'),
+            ]);
+            $this->info(sprintf(' Success creating user: %s and his email %s.', $name, $mail));
+            $bar->advance();
+        }
+        $bar->finish();
     }
 }
