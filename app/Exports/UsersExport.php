cc5230225dc60682286ec0feb0c6d52131c08161
moustaphahcse <moustapha.camara@helloce.fr>
2023-12-29 16:44:49 +0100

    feat(exportCSV): export with formatting

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -6,17 +6,19 @@ use App\Models\User;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 use Maatwebsite\Excel\Concerns\WithHeadings;
-use Maatwebsite\Excel\Concerns\WithMappedCells;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class UsersExport implements FromCollection, ShouldAutoSize, WithHeadings, WithMappedCells
+class UsersExport implements FromCollection, ShouldAutoSize, WithHeadings, WithColumnFormatting, WithMapping
 {
     /**
      * @return Collection
      */
     public function collection(): Collection
     {
-        return User::all();
+        return User::all(['id', 'name', 'email', 'created_at']);
     }
 
     public function headings(): array
@@ -24,11 +26,23 @@ class UsersExport implements FromCollection, ShouldAutoSize, WithHeadings, WithM
         return ["id", "Full name", "email", "Registered"];
     }
 
-    public function mapping(): array
+    public function map($row): array
     {
         return [
-            'name' => 'B1',
-            'email' => 'B2',
+            $row->id,
+            $row->name,
+            $row->email,
+            $row->created_at,
+
+        ];
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'B' => NumberFormat::FORMAT_GENERAL,
+            'C' => NumberFormat::FORMAT_GENERAL,
+            'D' => NumberFormat::FORMAT_DATE_YYYYMMDD,
         ];
     }
 }
