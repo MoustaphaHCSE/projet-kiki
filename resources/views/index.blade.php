b84095a4943dc861f6bb61dd105154b58b656c97
MoustaphaHCSE <moustapha.camara@helloce.fr>
2024-01-05 15:49:12 +0100

    feat(tests): remove useless code that didn't pass ExampleTest

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,7 @@
 @extends('layouts.default')
 @section('content')
-    @php
-        use App\Models\Celebrity;use App\Models\Movie;
-               $celebrities = Celebrity::all();
-               $movies = Movie::all();
-    @endphp
     <h1>Welcome to Projet Kiki</h1>
-    <img src="https://s.yimg.com/ny/api/res/1.2/dIu9XWkQs2bU9T7rm5ir9w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM3Ng--/https://media.zenfs.com/fr/purebreak_news_fr_110/1b225a9d1ef14a39b5a023971384528d"
-         alt="projet kiki" style="width:100%">
+    <img
+        src="https://s.yimg.com/ny/api/res/1.2/dIu9XWkQs2bU9T7rm5ir9w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM3Ng--/https://media.zenfs.com/fr/purebreak_news_fr_110/1b225a9d1ef14a39b5a023971384528d"
+        alt="projet kiki" style="width:100%">
 @stop
