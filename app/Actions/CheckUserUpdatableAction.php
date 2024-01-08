d5a76e8ffcb67c64fe22f7f88f0907d3664be9c9
MoustaphaHCSE <moustapha.camara@helloce.fr>
2024-01-08 11:14:03 +0100

    feat(controllers): action wording

--- app/Actions/CheckUserUpdatableAction.php
+++ app/Actions/CheckUserUpdatableAction.php
@@ -12,8 +12,8 @@ class CheckUserUpdatableAction
             return abort(403, 'TU PEUX NI TE SUPPRIMER NI SUPPRIMER UN SUPER ADMIN');
         }
         if ($action == "edit" && $user->hasRole('Super Admin') && $user->id != auth()->user()->id) {
-            return abort(403, 'A SUPER ADMIN CAN\'T UPDATE ANOTHER SUPER ADMIN');
+            return abort(403, 'UN SUPER ADMIN NE PEUT EN MODIFIER UN AUTRE');
         }
-        return new HttpException(200, "User can be updated");
+        return new HttpException(200, "L'utilisateur peut être modifié.");
     }
 }
