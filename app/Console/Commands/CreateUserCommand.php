fb656a3536797d9e41cb0a36980c535c1096ccdc
MoustaphaHCSE <moustapha.camara@helloce.fr>
2024-01-09 14:27:49 +0100

    feat(commands): create user instead of seeding db

--- app/Console/Commands/CreateUserCommand.php
+++ app/Console/Commands/CreateUserCommand.php
@@ -2,7 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Enums\RoleEnum;
 use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
@@ -14,8 +13,7 @@ class CreateUserCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'user:create {--count=}';
-
+    protected $signature = 'user:create {name} {email} {password} {roles}';
     /**
      * The console command description.
      *
@@ -26,26 +24,18 @@ class CreateUserCommand extends Command
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle(): void
     {
-        $count = $this->option('count');
-
-        $bar = $this->output->createProgressBar($count);
-        $bar->start();
-        for ($i = 1; $i <= $count; $i++) {
-//            $name = $this->argument('name');
-//            $email = $this->argument('email');
-//            $password = $this->argument('password') ?? Hash::make('password');
-            $name = fake()->name();
-            $mail = preg_replace('/[^a-z]/', '', strtolower($name));
-            User::create([
-                'name' => $name,
-                'email' => $mail . '@gmail.com',
-                'password' => Hash::make('password'),
-            ])->assignRole(RoleEnum::USER);
-            $this->info(sprintf(' Success creating user: %s and his email %s.', $name, $mail));
-            $bar->advance();
-        }
-        $bar->finish();
+        $name = $this->argument('name');
+        $email = $this->argument('email');
+        $password = $this->argument('password');
+        $roles = $this->argument('roles');
+        User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+            'roles' => $roles,
+        ]);
+        $this->info(sprintf('Success creating user: %s with the email %s.', $name, $email));
     }
 }
