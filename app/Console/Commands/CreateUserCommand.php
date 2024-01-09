7018543b18e990e4a3b50f9db4e64ef55d9ab012
MoustaphaHCSE <moustapha.camara@helloce.fr>
2024-01-09 14:54:09 +0100

    feat(commands): assign User role to newly created

--- app/Console/Commands/CreateUserCommand.php
+++ app/Console/Commands/CreateUserCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Enums\RoleEnum;
 use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
@@ -13,7 +14,7 @@ class CreateUserCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'user:create {name} {email} {password} {roles}';
+    protected $signature = 'user:create {name} {email} {password}';
     /**
      * The console command description.
      *
@@ -29,13 +30,12 @@ class CreateUserCommand extends Command
         $name = $this->argument('name');
         $email = $this->argument('email');
         $password = $this->argument('password');
-        $roles = $this->argument('roles');
         User::create([
             'name' => $name,
             'email' => $email,
             'password' => Hash::make($password),
-            'roles' => $roles,
-        ]);
+        ])->assignRole(RoleEnum::USER);
+
         $this->info(sprintf('Success creating user: %s with the email %s.', $name, $email));
     }
 }
